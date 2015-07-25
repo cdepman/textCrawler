cba6a1bfcb828034cbb7227e84828a11160376ba
Charlie Depman <cdepman@gmail.com>
2015-07-24 23:29:55 -0700

    starts work on fillToTail

--- textCrawler.js
+++ textCrawler.js
@@ -1,39 +1,69 @@
 
 var TextCrawler = function(action, className, speed){
 
-  var textLength = $('.' + className).length;
-  var hash = {color: ''};
+  var TEXTLENGTH = $('.' + className).length;
+  var HASH = {color: ''};
+  var STOPSHIFT = 0;
+  var CURRENT_POSITION = 0;
     
-  this.crawl = function(element){
-    element = element || $('.head')[0];
-    $(element).css('color', '#' + hash.color);
-    if ($(element).next().length === 0){
+  this.crawl = function(node){
+    CURRENT_POSITION++
+    var node = node || $('.head')[0];
+    $(node).css('color', '#' + HASH.color);
+    if ($(node).next().length === 0){
       var that = this;
       setTimeout(function(){
-        that.changeColor(hash.color);
+        that.changeColor(HASH.color);
+        CURRENT_POSITION = 0;
         that.crawl($('.head'));
       }, speed);
       return;
     }
     var that = this;
     setTimeout(function(){
-      that.crawl($(element).next()[0]);
+      that.crawl($(node).next()[0]);
     }, speed);
   };
 
   this.changeColor = function(oldColor){
-    oldColor = oldColor || '';
-    hash.color = Math.floor(Math.random()*16777215).toString(16);
-    if (hash.color === oldColor){
+    var oldColor = oldColor || '';
+    HASH.color = Math.floor(Math.random()*16777215).toString(16);
+    if (HASH.color === oldColor){
       this.changeColor();
     }
   }
 
-  this[action]();
+  this.fillToTail = function(node){
+    CURRENT_POSITION = ++CURRENT_POSITION;
+    if (STOPSHIFT === TEXTLENGTH){
+      STOPSHIFT = 0;
+      this.changeColor(HASH.color);
+    }
+    var node = node || $('.head')[0];
+    $(node).css('color', '#' + HASH.color);
+    if ($(node).next().length === 0){
+      var that = this;
+      setTimeout(function(){
+        CURRENT_POSITION = 0;
+        that.fillToTail($('.head'));
+      }, speed);
+      return;
+    }
+    var that = this;
+    setTimeout(function(){
+      if (CURRENT_POSITION <= TEXTLENGTH - STOPSHIFT){
+        that.fillToTail($(node).next()[0]);
+      } else {
+        CURRENT_POSITION = 0;
+      }
+    }, speed); 
+  }
+
   this.changeColor();
+  this[action]();
 };
 
 
 
 
-textCrawler = new TextCrawler('crawl', 'title', 50);
+textCrawler = new TextCrawler('fillToTail', 'title', 50);
