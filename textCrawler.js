1c7a2cf246ca0f68f915a5a9673191750adcc65f
Charlie Depman <cdepman@gmail.com>
2015-07-24 22:40:51 -0700

    improves color changing

--- textCrawler.js
+++ textCrawler.js
@@ -1,31 +1,34 @@
 
-  var textCrawler = function(className, flowSpeed){
+var textCrawler = function(className, flowSpeed){
 
-    var textLength = $('.' + className).length;
-    var hash = {color: ''};
-    changeColor();
-      
-    var crawl = function(element){
-      element = element || $('.head')[0];
-      $(element).css('color', '#' + hash.color);
-      if ($(element).next().length === 0){
-        setTimeout(function(){
-          crawl($('.head'));
-        }, flowSpeed);
-        return;
-      }
+  var textLength = $('.' + className).length;
+  var hash = {color: ''};
+  changeColor();
+    
+  var crawl = function(element){
+    element = element || $('.head')[0];
+    $(element).css('color', '#' + hash.color);
+    if ($(element).next().length === 0){
       setTimeout(function(){
-        crawl($(element).next()[0]);
+        changeColor(hash.color);
+        crawl($('.head'));
       }, flowSpeed);
-    };
-
-    function changeColor(){
-      hash.color = Math.floor(Math.random()*16777215).toString(16);
+      return;
     }
-
-    crawl();
+    setTimeout(function(){
+      crawl($(element).next()[0]);
+    }, flowSpeed);
   };
 
+  function changeColor(oldColor){
+    oldColor = oldColor || '';
+    hash.color = Math.floor(Math.random()*16777215).toString(16);
+    if (hash.color === oldColor){
+      changeColor();
+    }
+  }
 
+  crawl();
+};
 
-  textCrawler('title', 100);
+textCrawler('title', 50);
