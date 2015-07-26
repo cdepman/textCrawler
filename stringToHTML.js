996625c93337e89e0614ede51957a6b306984c0a
Charlie Depman <cdepman@gmail.com>
2015-07-26 02:21:50 -0700

    improves string to HTML functionality

--- stringToHTML.js
+++ stringToHTML.js
@@ -1,26 +1,10 @@
-var specialChars = {' ': '&nbsp'};
-
-var stringToHTML = function(string, element, containerOptions, letterOptions){
-  containerOptions = containerOptions || {};
-  letterOptions = letterOptions || {};
-
-	var HTMLStringBuilder = createOpeningTag(element, containerOptions);
-	for (var i = 0; i < string.length; i++){
-		var char = string[i];
-		if (specialChars[char]){
-			char = specialChars[char];
-		}
-		HTMLStringBuilder += createOpeningTag('div', letterOptions) + char + createClosingTag('div');
-	}
-	return HTMLStringBuilder + createClosingTag(element);
-}
-
-
 var createClosingTag = function(elementName){
+  elementName = elementName || 'div';
   return '</' + elementName + '>';
 }
 
 var createOpeningTag = function(elementName, options){
+  elementName = elementName || 'div';
   var openingTag = '<' + elementName;
   for (key in options){
     if (options[key]){
@@ -28,4 +12,22 @@ var createOpeningTag = function(elementName, options){
     }
   }
   return openingTag + '>';
+}
+
+
+var HTMLCharacterCode = function(char){
+   return '&#' + char.charCodeAt(0);
+}
+
+var stringToHTMLContainer = function(string, containerOptions, letterOptions){
+  element = containerOptions['element'] || 'div';
+  console.log('hello:', element);
+  containerOptions = containerOptions || {'class': 'container'};
+  letterOptions = letterOptions || {};
+
+	var HTMLStringBuilder = createOpeningTag(element, containerOptions);
+	for (var i = 0; i < string.length; i++){
+		HTMLStringBuilder += createOpeningTag('div', letterOptions) + HTMLCharacterCode(string[i]) + createClosingTag('div');
+	}
+	return HTMLStringBuilder + createClosingTag(element);
 }
